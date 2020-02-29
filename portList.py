4d5050c5ebcca97b40e39f04c5f8c45b88a4df2f
NguyenTienNam15 <56922601+NguyenTienNam15@users.noreply.github.com>
2020-02-29 14:33:52 +0700

    first commit

--- portList.py
+++ portList.py
@@ -4,17 +4,10 @@ import serial
 
 
 def serial_ports():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
+        
         ports = glob.glob('/dev/tty[A-Za-z]*')
     elif sys.platform.startswith('darwin'):
         ports = glob.glob('/dev/tty.*')
@@ -31,5 +24,4 @@ def serial_ports():
             pass
     return result
 
-ser = serial_ports()
-print (ser)
+
